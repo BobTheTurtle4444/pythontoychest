7dbd4fa64e1d3e722a2b725e99340268f5e54c5d
BobTheTurtle4444 <dida.vasu@gmail.com>
2016-07-12 11:02:02 -0700

    Create eg9.py

---
+++ eg9.py
@@ -0,0 +1,11 @@
+# 9.py - this program illustrates stacks in Python. 
+stack = [3,4,5]
+ print(stack)
+stack.pop()
+ print(stack)
+stack.append(40)
+ print(stack)
+stack.pop()
+ print(stack)
+stack.pop()
+ print(stack)
