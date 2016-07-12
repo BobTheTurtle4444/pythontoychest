766a7be92be7fbe3152b0bf0022221d8286d774a
BobTheTurtle4444 <dida.vasu@gmail.com>
2016-07-12 10:43:21 -0700

    Create eg3.py

---
+++ eg3.py
@@ -0,0 +1,8 @@
+# 3.py - this program illustrates variables, elif, and else in Python
+x = int(input("Please enter an integer: "))
+if x < 0:
+   print('Negative number\n') 
+elif x == 0:
+   print('Zero \n')
+else:
+   print('positive number\n')
