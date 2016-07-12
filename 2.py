fd375a757108d58877569ea7774d44342f4f1934
BobTheTurtle4444 <dida.vasu@gmail.com>
2016-07-12 10:33:10 -0700

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,11 @@
+# 2.py - this program illustrates lists, append, reverse, and remove in Python.
+list = ['physics', 'chemistry', 1997, 2000]
+print(list)
+list.append(80)
+print(list)
+list.insert(0,80)
+print(list)
+list.remove('chemistry')
+print(list)
+list.reverse()
+print(list)
