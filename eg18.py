24e00946e01b055f21934cb9d739f398685f36e8
BobTheTurtle4444 <dida.vasu@gmail.com>
2016-07-12 11:45:16 -0700

    Create eg18.py

---
+++ eg18.py
@@ -0,0 +1,10 @@
+# 18.py - this program illustrates Python except handling using try, except, and finally.
+try:
+    x = float(input("Your number: "))
+    inverse = 1.0 / x
+except ValueError:
+    print("You should have given either an int or a float")
+except ZeroDivisionError:
+    print("Infinity")
+finally:
+    print("Inside Finally block.")
