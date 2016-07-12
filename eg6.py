ea6c58018d8a41956ac5fd31205830b9b3711534
BobTheTurtle4444 <dida.vasu@gmail.com>
2016-07-12 10:56:34 -0700

    Create eg6.py

---
+++ eg6.py
@@ -0,0 +1,6 @@
+# 6.py - this program illustrates function, pefinition, and parameters.
+def happyBirthday(person):
+    print("Happy Birthday, dear " + person + ".")
+happyBirthday('Emily')
+happyBirthday('Andre')
+
