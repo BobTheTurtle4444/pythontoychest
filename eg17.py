75243cf8634b49763ea559fee86c31780747c894
BobTheTurtle4444 <dida.vasu@gmail.com>
2016-07-12 11:31:47 -0700

    Create eg17.py

---
+++ eg17.py
@@ -0,0 +1,9 @@
+# 17.py - this program illustrates break and continue in Python. 
+for letter in 'Python': 
+   if letter == 'o':
+      break
+   if letter == 'y': 
+      continue
+   print ('Current Letter :', letter)
+ 
+print ("Good bye!")
