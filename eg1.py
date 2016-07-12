c8f0063ea55b0f50bb6b5a01fe4dba35cd061cbc
BobTheTurtle4444 <dida.vasu@gmail.com>
2016-07-12 10:25:22 -0700

    Create eg1.py

---
+++ eg1.py
@@ -0,0 +1,8 @@
+# 1.py - this program illustrates lists in Python
+alist = ["Hello", "World", 1997, 2001]
+print(alist[2])
+alist[2] = 2002
+print(alist[2])
+del alist[2]
+print("After deleting value at index 2 : ")
+print(alist)
