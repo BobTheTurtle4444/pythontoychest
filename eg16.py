782351f489e7017fe5374c45709fa279fe59a198
BobTheTurtle4444 <dida.vasu@gmail.com>
2016-07-12 11:08:33 -0700

    Create eg16.py

---
+++ eg16.py
@@ -0,0 +1,7 @@
+# 16.py - this program illustrates the use of while in Python.
+temperature = 115  
+while temperature > 112: # first while loop code
+    print(temperature)
+    temperature = temperature - 1
+ 
+print('The tea is cool enough.')
