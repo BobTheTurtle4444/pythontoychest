366a05a8317b855d6a875247f3ac8cf01bb9e0bf
BobTheTurtle4444 <dida.vasu@gmail.com>
2016-07-12 11:03:18 -0700

    Create eg10.py

---
+++ eg10.py
@@ -0,0 +1,11 @@
+# 10.py - this program illustrates queue in Python. 
+deque(["John", "Jacob", "Jingle"]) 
+print(queue)
+queue.append("hymer")
+queue.append("schmidt")
+print(queue)
+queue.popleft()
+print(queue)
+queue.popleft()
+print(queue)
+
